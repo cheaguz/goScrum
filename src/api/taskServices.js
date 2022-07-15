d12a5fea518bd59cb409de7338b0f638a93ac75d
Cheaguz <aguzf83@gmail.com>
2022-07-15 16:56:42 -0300

    minor changes

--- src/api/taskServices.js
+++ src/api/taskServices.js
@@ -16,6 +16,7 @@ export const newTask = ({ title, status, importance, description }) => {
             },
         }),
     })
+    .then(response => response.json())
 };
 
 export const getFormData = () => {
